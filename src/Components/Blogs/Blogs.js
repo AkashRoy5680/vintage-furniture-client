7b13f923e45dc99624a49adeb265a4c87446b4fc
AkashRoy5680 <royakash1272@gmail.com>
2022-07-08 02:35:46 +0200

    modified blog section

--- src/Components/Blogs/Blogs.js
+++ src/Components/Blogs/Blogs.js
@@ -10,7 +10,7 @@ const Blogs = () => {
          <h4> Q1:Difference between javascript and nodejs?</h4>
            <p> JavaScript is a lightweight, cross-platform, interpreted scripting programming language that is primarily used for client-side scripting. It's built into both Java and HTML. On the other hand, Node. js is a server-side scripting language based on the Google Chrome V8 engine. </p> <br/>
           <h4>Q2: When should you use nodejs and when should you use mongodb?</h4>
-          <p>MongoDB and NodeJS are two different technologies. MonogDB is a database system which gives you a chance to efficiently store documents in a database and to perform operations like data updates, or to search documents by some criterias. On Contrary, Nodejs is a Javascript engine that you can write any application you want with (by programming in the Javascript language). It runs your Javascript code. Most commonly, it is used to build servers that can respond to web requests.</p><br/>
+          <p>MongoDB and NodeJS are two different technologies. MonogDB is a database system which gives you a chance to efficiently store documents in a database and to perform operations like data updates, or to search documents by some criterias. On Contrary, Nodejs is a Javascript engine that you can write any application you want with (by programming in the Javascript language). It runs your Javascript code. Most commonly, it is used to build  that can respond to web requests.</p><br/>
             
          <h4>Q3: Differences between sql and nosql databases.?</h4>
          <p> In NOSQL, Individual records are stored as documents.Documents belonging to a particular class or group as a collection.It means pre-defined structered for incoming data can be defined.
